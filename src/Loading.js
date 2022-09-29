d91e57c7915e48dd131d1930bf49e3f37ead0ccb
Kaosi Ezealigo <ezealigokosiso@gmail.com>
2022-09-29 20:04:30 +0100

    changes background if isLoading is true

--- src/Loading.js
+++ src/Loading.js
@@ -1,9 +1,14 @@
 import React from "react";
 
-export const Loading = () => {
+export const Loading = ({ isLoading }) => {
+  if (isLoading) {
+    document.body.style.background = "#a3a3a3";
+  }
   return (
     <>
-      <h2>Loading...</h2>
+      <div className="loading">
+        <h2>Loading...</h2>
+      </div>
     </>
   );
 };
